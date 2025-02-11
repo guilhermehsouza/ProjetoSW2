9b884213defc800f29a09966ac51b30a562005ee
Guilherme Henrique de Souza <guihenry@gmail.com>
2025-02-11 10:47:53 -0300

    11/02

--- index.php
+++ index.php
@@ -12,7 +12,31 @@
 </head>
 <body>
     
-    <h1>Projeto SW2</h1>
+    <div class="container-fluid bg-primary text-white text-center p-5">
+        <h1>Projeto SW2</h1>
+        <p>
+            Elaborado por Guilherme Henrique de Souza
+        </p>
+    </div>
+
+    <div class="container">
+        <div class="row">
+
+            <div class="col-sm-4"> 
+                <h1>Administrador</h1>
+            </div>
+
+            <div class="col-sm-4"> 
+                <h1>Categorias</h1>
+            </div>
+
+            <div class="col-sm-4"> 
+                <h1>Fornecedor</h1>
+            </div>
+
+        </div>
+    </div>
+
 
 </body>
 </html>
